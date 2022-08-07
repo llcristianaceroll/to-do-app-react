921a953bf5cbce485af362e9ab89adbffab1fa8c
Cristian Acero <llcristianaceroll@gmail.com>
2022-08-07 12:25:51 -0500

    complete and delete to-dos using methods and useState

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import TodoItem from "./TodoItem";
 import CreateToDoButton from "./CreateToDoButton";
 
 const defaultTodos = [
-  { text: "Go to the gym", completed: true },
+  { text: "Go to the gym", completed: false },
   { text: "Fix bug", completed: false },
-  { text: "Study English", completed: false},
+  { text: "Study English", completed: false },
 ];
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearchValue] = React.useState('');
-  const completedTodos = todos.filter(todo => todo.completed).length;
+  const [searchValue, setSearchValue] = React.useState("");
+  const completedTodos = todos.filter((todo) => todo.completed).length;
   const totalTodos = todos.length;
 
   let searchedTodos = [];
@@ -22,15 +22,29 @@ function App() {
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
   } else {
-  
-    searchedTodos = todos.filter(todo => {
+    searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
-    })
-    console.log(searchedTodos)
+    });
   }
 
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    // newTodos[todoIndex].completed = true;
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed; // with this become toggle when we clic again
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+    /* const newTodos = todos.filter(todo => todo.text !== text)
+    setTodos(newTodos) // other way to deleted to-dos */
+  };
 
   return (
     <React.Fragment>
@@ -42,6 +56,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,29 +2,22 @@ import React from "react";
 import "./TodoItem.css";
 
 const TodoItem = (props) => {
-  const onComplete = () => {
-    alert(`Ya completaste el todo ${props.text}`);
-  };
-
-  const onDelete = () => {
-    alert(`Borraste el todo ${props.text}`);
-  };
-  return (
-    <li className="TodoItem">
-      <span
-        onClick={onComplete}
-        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-      >
-        √
-      </span>
-      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-        {props.text}
-      </p>
-      <span onClick={onDelete} className="Icon Icon-delete">
-        X
-      </span>
-    </li>
-  );
+    return (
+        <li className="TodoItem">
+            <span
+                onClick={props.onComplete}
+                className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+            >
+                {props.completed ? "✅ " : "⛔️"}
+            </span>
+            <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+                {props.text}
+            </p>
+            <span onClick={props.onDelete} className="Icon Icon-delete">
+                X
+            </span>
+        </li>
+    );
 };
 
 export default TodoItem;
