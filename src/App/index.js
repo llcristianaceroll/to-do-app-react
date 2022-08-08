9563047716f849f2827a6e28a26ae3f52ab68c4b
Cristian Acero <llcristianaceroll@gmail.com>
2022-08-07 20:11:38 -0500

    use of local storage to persist data( to-dos)

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import AppUI from "./AppUI";
 
-const defaultTodos = [
-  { text: "Go to the gym", completed: false },
-  { text: "Fix bug", completed: false },
-  { text: "Study English", completed: false },
-];
+// const defaultTodos = [
+//   { text: "Go to the gym", completed: false },
+//   { text: "Fix bug", completed: false },
+//   { text: "Study English", completed: false },
+// ];
+
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TO_DOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TO_DOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
+
   const completedTodos = todos.filter((todo) => todo.completed).length;
   const totalTodos = todos.length;
 
@@ -24,19 +36,24 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringrifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TO_DOS_V1', stringrifiedTodos);
+    setTodos(newTodos);
+  }
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     // newTodos[todoIndex].completed = true;
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed; // with this become toggle when we clic again
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
     /* const newTodos = todos.filter(todo => todo.text !== text)
     setTodos(newTodos) // other way to deleted to-dos */
   };
